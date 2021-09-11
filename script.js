bb9c1cd2f5c811f1346b912e01c4f4f3933b6bd8
Hernan Di Tano <hditano@gmail.com>
2021-09-11 19:43:18 -0300

    attached button to eventListener

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const body = document.querySelector('body');
 const div = document.createElement('div');
+const h1 = document.createElement('h1');
+const triggerButton = document.createElement('button');
 body.appendChild(div)
-const img = document.createElement('img');
-const addImg = div.appendChild(img)
+div.appendChild(h1);
+div.appendChild(triggerButton);
+triggerButton.textContent = 'New Idea';
+// const img = document.createElement('img');
+// const addImg = div.appendChild(img)
 
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(Response => Response.json())
-    .then(data => img.setAttribute('src', `${data.message}`));
+triggerButton.addEventListener('click', function () {
+
+    fetch('https://apis.scrimba.com/bored/api/activity')
+        .then(Response => Response.json())
+        .then(data => h1.textContent = `Idea is: ${data.activity}`);
+
+})
