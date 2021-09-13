5ea84c630ece2ded468c5d2e0884584dc3e7d07f
Hernan Di Tano <hditano@gmail.com>
2021-09-13 07:32:41 -0300

    add basic logic to loop through an API

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const body = document.querySelector('body');
 const div = document.createElement('div');
 const h1 = document.createElement('h1');
 const triggerButton = document.createElement('button');
-const fetchArray = document.querySelector('fetch-array');
 const p = document.createElement('p');
 body.appendChild(div)
 div.appendChild(h1);
@@ -14,12 +13,26 @@ const newArray = [];
 // const img = document.createElement('img');
 // const addImg = div.appendChild(img)
 
-triggerButton.addEventListener('click', function () {
-    fetch('https://apis.scrimba.com/jsonplaceholder/users')
-        .then(Response => Response.json())
+// triggerButton.addEventListener('click', function () {
+//     fetch('https://apis.scrimba.com/jsonplaceholder/users')
+//         .then(Response => Response.json())
+//         .then(data => {
+//             const newArray = data.slice(0,5);
+//             p.textContent = newArray
+//         })
+// })
+
+
+triggerButton.addEventListener('click', function() {
+    fetch('https://apis.scrimba.com/jsonplaceholder/posts', { method: 'GET' })
+        .then(res => res.json())
         .then(data => {
-            const newArray = data.slice(0,5);
-            console.log(newArray);
+            const postArray = data.slice(0,5);
+            let html = '';
+            for(let post of postArray) {
+                html += `<h1>${post.title} </h1> 
+                        <p>${post.body}</p>`
+            }
+            div.innerHTML = html;
         })
 })
-
