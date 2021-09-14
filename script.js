499f2055ed57d052d1cdebba6f72ac4f32283ce8
Hernan Di Tano <hditano@gmail.com>
2021-09-14 19:28:48 -0300

    refactor code

--- script.js
+++ script.js
@@ -1,67 +1,52 @@
 const body = document.querySelector('body');
 const div = document.createElement('div');
-const h1 = document.createElement('h1');
-const triggerButton = document.createElement('button');
 const submitBtn = document.querySelector('.submit-btn');
-const textTitle = document.querySelector('#text-title');
-const textArea = document.querySelector('#text-area');
-const p = document.createElement('p');
 body.appendChild(div)
-div.appendChild(h1);
-div.appendChild(p);
-div.appendChild(triggerButton);
-triggerButton.textContent = 'Show Posts';
-const newArray = [];
-
-// const img = document.createElement('img');
-// const addImg = div.appendChild(img)
-
-// triggerButton.addEventListener('click', function () {
-//     fetch('https://apis.scrimba.com/jsonplaceholder/users')
-//         .then(Response => Response.json())
-//         .then(data => {
-//             const newArray = data.slice(0,5);
-//             p.textContent = newArray
-//         })
-// })
+let postsArrays = []
 
 
-triggerButton.addEventListener('click', function () {
-    fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
-            method: 'GET'
-        })
-        .then(res => res.json())
-        .then(data => {
-            const postArray = data.slice(0, 5);
-            let html = '';
-            for (let post of postArray) {
-                html += `<h3>${post.title} </h3> 
-                        <p>${post.body}</p>
-                        <hr />`
-            }
-            div.innerHTML = html;
-        })
-})
+
+
+
+fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+        method: 'GET'
+    })
+    .then(res => res.json())
+    .then(data => {
+        postsArrays = data.slice(0, 5);
+        renderPosts();
+    })
 
 
 // Will trigger the submit button.
-// Line 52: will create an object containing the values that will be used on the fetch.
-// Line 58: Will create options that goes along with the fetch function
-// line 71: Will post everything to the API, adding whatever we wrote on the form to be sent to the API, along to adding to the List already created
+// Line 41: will create an object containing the values that will be used on the fetch.
+// Line 47: Will create options that goes along with the fetch function
+// line 55: Will post everything to the API, adding whatever we wrote on the form to be sent to the API, along to adding to the List already created
 
+function renderPosts() {
+    let html = '';
+    for (let post of postsArrays) {
+        html += `<h3>${post.title} </h3> 
+                <p>${post.post}</p>
+                <hr />`
+    }
+    div.innerHTML = html;
+}
 
 submitBtn.addEventListener('click', function (e) {
     e.preventDefault()
+    const textTitle = document.getElementById('text-title').value;
+    const textArea = document.getElementById('text-area').value;
+
     const data = {
-        title: `${textTitle.value}`,
-        post: `${textArea.value}`,
+        title: textTitle,
+        post: textArea
     }
+
 
     const options = {
         method: 'POST',
-        body: JSON.stringify({
-            data
-        }),
+        body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json'
         }
@@ -70,12 +55,7 @@ submitBtn.addEventListener('click', function (e) {
     fetch('https://apis.scrimba.com/jsonplaceholder/posts', options)
         .then(res => res.json())
         .then(post => {
-            div.innerHTML = `<h3>${textTitle.value} </h3> 
-                        <p>${textArea.value}</p>
-                        <hr />
-                        ${div.innerHTML}`
+            postsArrays.unshift(post);
+            renderPosts()
         })
 })
-
-
-// Fetch that will use method 'POST' to send data to the API
